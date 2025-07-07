0424f46b8e6a2fec7c2af9175d0611ee2775576b
Atom1l <atom7548@gmail.com>
2025-07-07 13:59:07 +0700

    Updated LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -23,7 +23,7 @@ export default function LandingPage() {
   };
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white overflow-hidden">
       <Navigation />
       
       {/* Hero Section */}
